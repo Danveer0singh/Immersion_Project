7ab105332ccc2734ebd092ec3a270b9b5bfa87fb
Danveer0singh <danveersingh0408@gmail.com>
2025-07-14 09:23:35 +0530

    Weather_Map_Added

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/App.css';
+import 'leaflet/dist/leaflet.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
